b4c36790ce20c3681a1ff0226cdd816b5dbc6c6a
surajpsharma <surajsharma6363mumbai@gmail.com>
2025-10-16 15:28:27 +0530

    Updated app.js with latest changes

--- app.js
+++ app.js
@@ -16,9 +16,12 @@ const startServer = async () => {
     await connectDB();
     await createUsersTable();
 
+    app.get("/", (req, res) => {
+      res.send("API is running...");
+    });
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error.message);
